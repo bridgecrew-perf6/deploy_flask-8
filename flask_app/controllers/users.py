1089ae76cf0adc4caade7972677b6e27cef62fba
xtina-lt <92126039+xtina-lt@users.noreply.github.com>
2022-02-07 01:54:02 -0500

    more dynamic and semantic

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -12,18 +12,20 @@ def home():
     # [{'id': 435033175, 'node_id': 'R_kgDOGe4UVw', 'name': 'bank_first_national_north_pole', 'full_name': 'xtina-lt/bank_first_national_north_pole', 'private': False, 'owner': {'login': 'xtina-lt', 'id': 92126039, 'node_id': 'U_kgDOBX27Vw', 'avatar_url': 'https://avatars.githubusercontent.com/u/92126039?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/xtina-lt', 'html_url': 'https://github.com/xtina-lt', 'followers_url': 'https://api.github.com/users/xtina-lt/followers', 'following_url': 'https://api.github.com/users/xtina-lt/following{/other_user}', 'gists_url': 'https://api.github.com/users/xtina-lt/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/xtina-lt/starred{/owner}{/repo}', 'subscriptions_url': 'https://api.github.com/users/xtina-lt/subscriptions', 'organizations_url': 'https://api.github.com/users/xtina-lt/orgs', 'repos_url': 'https://api.github.com/users/xtina-lt/repos', 'events_url':
     count = 0 # to count dictionaries
     lst=[]
-    # empty list to add dictionaries of output
-    for i in output:
-        print(i)
-        count += 1;
-        data={
-        # for every i in json request
-            "name" : i['name'],
-            "language": i['language'],
-            "description": i['description'],
-            "url": i['html_url']
-        }
-        lst.append(data)
+    print(lst)
+    if output == 'message':
+        # empty list to add dictionaries of output
+        for i in output:
+            print(i)
+            count += 1;
+            data={
+            # for every i in json request
+                "name" : i['name'],
+                "language": i['language'],
+                "description": i['description'],
+                "url": i['html_url']
+            }
+            lst.append(data)
     return render_template('index.html', output = lst)
 
 @app.route('/test')
